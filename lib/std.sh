f3996779e02919ff9e72891dcf7bc7c262f23c7e
Lenin Valverde <leninvalve@gmail.com>
2026-09-18 18:41:56 -0500

    Fixed shebang, added readonly to constants, removed LAST_ERROR global

--- lib/std.sh
+++ lib/std.sh
@@ -1,39 +1,35 @@
-#!/usr/bin/bash
-
-HOSTS_DIR="/srv/clients"	# All clients's home here.
-HOSTS_DIR_PERMS="701"
-HOSTS_DIR_OWNER="root:root"
+readonly HOSTS_DIR="/srv/clients"	# All clients's home here.
+readonly HOSTS_DIR_PERMS="701"
+readonly HOSTS_DIR_OWNER="root:root"
 
 # For a single client:
-USER_DIR_PERMS="701"				# User's home dir. www, logs and related dirs live here.
-USER_DIR_OWNER="root:root" 	# User's home dir is owned by root. Required for ssh jail.
-USER_WWW_PERMS="750"				# www-data belongs to group (via ACL). Group can read and exec (dirs).
-USER_TMP_PERMS="700"				# For PHP session data.
-USER_LOGS_PERMS="700"				# nginx and php-fpm logs.
-USER_PRIVATE_PERMS="700"		# Secrets user may have live here.
+readonly USER_DIR_PERMS="701"				# User's home dir. www, logs and related dirs live here.
+readonly USER_DIR_OWNER="root:root" 	# User's home dir is owned by root. Required for ssh jail.
+readonly USER_WWW_PERMS="750"				# www-data belongs to group (via ACL). Group can read and exec (dirs).
+readonly USER_TMP_PERMS="700"				# For PHP session data.
+readonly USER_LOGS_PERMS="700"				# nginx and php-fpm logs.
+readonly USER_PRIVATE_PERMS="700"		# Secrets user may have live here.
 
-PHP_VERSION="8.4"
+readonly PHP_VERSION="8.4"
 
-LAST_ERROR=""
-
-log_err() { echo ">> ERROR: $1"; return 0; }
+log_err() { echo ">> ERROR: $1" >&2; return 0; }
 log_ok() { echo ">> OK: $1"; return 0; }
 
 pre_flight() {
 	local perms
 	local owner
 
-	[[ -d "$HOSTS_DIR" ]] || { LAST_ERROR="'$HOSTS_DIR' missing. Aborting."; return 1; }
+	[[ -d "$HOSTS_DIR" ]] || { log_err "'$HOSTS_DIR' missing. Aborting."; return 1; }
 	
 	perms=$(stat -c %a "$HOSTS_DIR")
-	[[ "$perms" == "$HOSTS_DIR_PERMS" ]] || { LAST_ERROR="'$HOSTS_DIR' wrong permissions: $perms. Expected: '$HOSTS_DIR_PERMS'"; return 1; }
+	[[ "$perms" == "$HOSTS_DIR_PERMS" ]] || { log_err "'$HOSTS_DIR' wrong permissions: $perms. Expected: '$HOSTS_DIR_PERMS'"; return 1; }
 
 	owner=$(stat -c %U:%G "$HOSTS_DIR")
-	[[ "$owner" == "$HOSTS_DIR_OWNER" ]] || { LAST_ERROR="'$HOSTS_DIR' wrong owner: $owner. Expected: '$HOSTS_DIR_OWNER'"; return 1; }
+	[[ "$owner" == "$HOSTS_DIR_OWNER" ]] || { log_er "'$HOSTS_DIR' wrong owner: $owner. Expected: '$HOSTS_DIR_OWNER'"; return 1; }
 
-	command -v setfacl &> /dev/null || { LAST_ERROR="Command 'setfacl' not installed. Aborting."; return 1; }
-	command -v php-fpm${PHP_VERSION} &> /dev/null || { LAST_ERROR="PHP-FPM $PHP_VERSION not installed. Aborting."; return 1; }
-	command -v mariadb &> /dev/null || { LAST_ERROR="MariaDB not installed. Aborting."; return 1; }
+	command -v setfacl &> /dev/null || { log_err "Command 'setfacl' not installed. Aborting."; return 1; }
+	command -v php-fpm${PHP_VERSION} &> /dev/null || { log_err "PHP-FPM $PHP_VERSION not installed. Aborting."; return 1; }
+	command -v mariadb &> /dev/null || { log_err "MariaDB not installed. Aborting."; return 1; }
 	
 	# All tests passed. Go ahead!
 	return 0
--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 set -euo pipefail
 
 [[ $EUID -eq 0 ]] || { echo "Run as root. Bye."; exit 1; }
 source ./lib/std.sh
 
-pre_flight || { log_err "$LAST_ERROR"; exit 1; }
+pre_flight || exit 1
 
 log_ok "Pre-flight in order."
